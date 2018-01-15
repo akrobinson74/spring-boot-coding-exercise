8b2f5c0247a87dbea80f5f491f480e9ffc4f5686
Adam Robinson <adam.robinson@rewe-digital.com>
2018-01-15 15:19:01 +0100

    initial commit of repo

---
+++ src/main/java/com/akrobinson/springbootcodingexercise/SpringBootCodingExerciseApplication.java
@@ -0,0 +1,12 @@
+package com.akrobinson.springbootcodingexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCodingExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCodingExerciseApplication.class, args);
+	}
+}
